0b09dd2945415b19d2b1f55df3a545f4e601ceb2
Sohail Alakhdar <sohailalakhdar@gmail.com>
2025-11-29 21:12:34 +0200

    uploadProject

--- src/commen/utils/email/email.event.ts
+++ src/commen/utils/email/email.event.ts
@@ -1,28 +1,32 @@
 import { EventEmitter } from 'node:events';
 import { sendEmail } from '../email/send.email';
 import Mail from 'nodemailer/lib/mailer';
-import { verifyEmailTemplate } from '../email/templates/verify.templete';
+import { verifyEmailTemplate } from './templates/verify.templete';
 
-interface IEmail extends Mail.Options {
-  otp: number;
+export interface IEmail extends Mail.Options {
+  otp: string;
 }
 export const emailEvent = new EventEmitter();
-
-emailEvent.on('ConfirmEmail', async (data: IEmail) => {
-  try {
-    data.html = verifyEmailTemplate({ otp: String(data.otp) });
-    data.subject = 'Confirm your email';
-    await sendEmail(data);
-  } catch (error) {
-    console.error('Error sending email:', error);
-  }
+emailEvent.on('ConfirmEmail', (data: IEmail) => {
+  void (async () => {
+    try {
+      data.html = verifyEmailTemplate({ otp: String(data.otp) });
+      data.subject = 'Confirm your email';
+      await sendEmail(data);
+    } catch (err) {
+      console.error('Error sending email (ConfirmEmail):', err);
+    }
+  })();
 });
-emailEvent.on('resetPassword', async (data: IEmail) => {
-  try {
-    data.html = verifyEmailTemplate({ otp: String(data.otp) });
-    data.subject = 'Reset your password';
-    await sendEmail(data);
-  } catch (error) {
-    console.error('Error sending email:', error);
-  }
+
+emailEvent.on('resetPassword', (data: IEmail) => {
+  void (async () => {
+    try {
+      data.html = verifyEmailTemplate({ otp: String(data.otp) });
+      data.subject = 'Reset your password';
+      await sendEmail(data);
+    } catch (err) {
+      console.error('Error sending email (resetPassword):', err);
+    }
+  })();
 });
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,29 +1,42 @@
 import { UserRepository } from './../../DB/repository/user.repository';
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
 import { IUser } from 'src/commen/interfaces/user.interface';
 import { SignupBodyDto } from './dto/signup.dto';
 import { HUserDocument } from 'src/DB';
 import { generateHash } from 'src/commen';
+import { emailEvent } from 'src/commen/utils/email';
 
 @Injectable()
 export class AuthenticationService {
   users: IUser[] = [];
-  constructor(private readonly userRepository:UserRepository
-  ) {}
+  constructor(private readonly userRepository: UserRepository) {}
   async signup(data: SignupBodyDto): Promise<HUserDocument> {
     const { username, email, password } = data;
     const checkUserExist = await this.userRepository.findOne({
       filter: {
-        email
-      }
+        email,
+      },
     });
-    if(checkUserExist){
-      throw new ConflictException("User Already Exist!")
+    if (checkUserExist) {
+      throw new ConflictException('User Already Exist!');
     }
     const [user] = await this.userRepository.create({
-      data:[{
-      username, email, password:await generateHash(password)
-    }]})
-    return user
+      data: [
+        {
+          username,
+          email,
+          password: await generateHash(password),
+        },
+      ],
+    });
+    if (!user) {
+      throw new BadRequestException('Error');
+    }
+    emailEvent.emit('ConfirmEmail', { to: email, otp: '6546321' });
+    return user;
   }
 }
